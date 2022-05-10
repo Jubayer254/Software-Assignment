f8d5e5acc4804eb14713f04f8a00f39b633ddbf6
abir <achowdhury191255@bscse.uiu.ac.bd>
2022-05-10 21:22:06 +0600

    Modify the program to Factory Pattern

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,33 +1,98 @@
-public class Calculator {
-    int result;
-    public int add(int num1, int num2){
-        result = num1+num2;
-        return result;
-    }
-    public int sub(int num1, int num2){
-        result = num1-num2;
-        return result;
-    }
-    public int mul(int num1, int num2){
-        result = num1*num2;
-        return result;
-    }
-    public float div(int num1, int num2){
-        result = num1/num2;
-        return result;
-    }
-    public float mod(int num1, int num2){
-        result = num1%num2;
-        return result;
-    }
 
-    public static void main(String[] args) {
-        Calculator c = new Calculator();
-        System.out.println("The Result of Addition: "+c.add(6,2));
-        System.out.println("The Result of Subtraction: "+c.sub(6,2));
-        System.out.println("The Result of Multiplication: "+c.mul(6,2));
-        System.out.println("The Result of Division: "+c.div(6,2));
-        System.out.println("The Result of Mod: "+c.mod(6,2));
-    }
+public interface Calculator {
+    void calculate();
+}
 
+class add implements Calculator
+{
+
+    public void calculate() {
+        float result = 6 + 2;
+    }
+	 
+}
+
+class sub implements Calculator
+{
+
+    public void calculate() {
+         float result = 6 + 2;
+    }
+	 
+}
+
+
+class mul implements Calculator
+{
+
+    public void calculate() {
+         float result = 6 + 2;
+    }
+    
+	 
+}
+
+
+class div implements Calculator
+{
+
+    public void calculate() {
+        float result = 6 + 2;
+    }
+	 
+}
+
+class mod implements Calculator
+{
+
+    public void calculate() {
+        float result = 6 + 2;
+    }
+	 
+}
+
+class CalculatorFactory
+{
+ 
+	 public Calculator getOparetion(String type) throws Exception
+	 {
+		 switch (type)
+		 {
+			 case "add":
+				 return new add();
+			 case "sub":
+				 return new sub();
+			 case "mul":
+				 return new mul();
+                         case "div":
+				 return new div();
+                         case "mod":
+				 return new mod();
+			 default:
+				 throw new Exception( "Calculator type : "+type+" cannot be instantiated");
+		 }
+	 }
+}
+
+class do_Oparetion
+{
+	 public static void main(String[] args) throws Exception
+	 {
+		 
+		 CalculatorFactory shapeFactory = new CalculatorFactory();
+		 Calculator add =shapeFactory.getOparetion("add");
+		 add.calculate();
+                 
+		 Calculator sub = shapeFactory.getOparetion("sub");
+		 sub.calculate();
+		 
+		 Calculator mul = shapeFactory.getOparetion("mul");
+		 mul.calculate();
+                 
+                 Calculator div = shapeFactory.getOparetion("div");
+		 div.calculate();
+		 
+		 Calculator mod= shapeFactory.getOparetion("mod");
+		 mod.calculate();
+	 }
 }
